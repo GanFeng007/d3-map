78259b663b8d60e8d547f946216892f0e6780f6e
feng <675198494@qq.com>
2021-02-28 20:13:58 +0800

    新增江西省的渲染

--- src/assets/js/baseMap.js
+++ src/assets/js/baseMap.js
@@ -2,86 +2,115 @@
 import * as d3 from 'd3'
 
 export default class BaseMap {
-    constructor(el, opts) {
-        this.d3Container = d3.selectAll(el)
-        this.opts = opts
-    }
-    init() {
-        d3.json(`./map/china.json`).then(geoJson => {
-            this.createSVG()
-            this.createMainGroup()
-            this.createMapGroup()
-            this.renderMap(geoJson)
-        }, e => {
-            reject(e)
-        })
-    }
-    // 创建svg容器
-    createSVG() {
-        this.svg = d3.selectAll('svg')
-        if (this.svg._groups && this.svg._groups[0][0]) {
-            return
-        }
-        let zoom = d3.zoom()
-        .scaleExtent([1, 8])
-        .on("zoom", ()=> this.zoomed());
-        this.svg = this.d3Container.append('svg')
-            .attr('width', this.opts.width)
-            .attr('height', this.opts.height)
-            .call(zoom)
-    }
-    // 创建主g容器 ，用于缩放
-    createMainGroup() {
-        this.mainGroup = this.svg.selectAll('g.main-group')
-        if (this.mainGroup._groups && this.mainGroup._groups[0][0]) {
-            return
-        }
-        this.mainGroup = this.svg.append('g')
-            .attr('class', 'main-group')
-    }
-    // 创建地图容器，存放地图相关path
-    createMapGroup() {
-        this.mapGroup = this.mainGroup.selectAll('g.map-group')
-        if (this.mapGroup._groups && this.mapGroup._groups[0][0]) {
-            return
-        }
-        this.mapGroup = this.mainGroup.append('g')
-            .attr('class', 'map-group')
-    }
-    // 渲染地图
-    renderMap(geoJson) {
-        this.projection = d3.geoMercator().fitSize([this.opts.width - 10, this.opts.height - 10], geoJson);
-        this.path = d3.geoPath()
-            .projection(this.projection);
-        this.mapGroup
-            .selectAll('path')
-            .data(geoJson.features)
-            .enter()
-            .append('g')
-            .attr('class', 'path-group')
-            .append('path')
-            .attr('class', 'map-path')
-            .attr('d', this.path)
-        this.mapGroup.selectAll('.path-group')
-            .append('text')
-            .text(d=>{
-                return d.properties.name
-            })
-            .attr('transform', d=>{
-                if(!d.properties.center) return
-                return `translate(${this.projection(d.properties.center)}),scale(0.8)`
-            })
-    }
-    // 缩放
-    zoomed() {
-        let t = d3.event.transform;
-        this.mainGroup.attr("transform", t);
-        this.mainGroup.selectAll("circle.point")
-            .transition()
-            .duration(750)
-            .attr("transform", function () {
-                let curr = this.transform.baseVal[0].matrix;
-                return "translate(" + curr.e + "," + curr.f + ")scale(" + 1 / t.k + ")";
-            });
-    }
+	constructor(el, opts) {
+		this.d3Container = d3.selectAll(el);
+		this.opts = opts;
+		this.level = 1;
+	}
+	init() {
+		this.createSVG()
+		this.createMainGroup()
+		this.worldGroup = this.createMapGroup('map-group');
+		this.jiangxiGroup = this.createMapGroup('map-group-jiangxi').attr('display','none');
+		d3.json(`./map/china.json`).then(geoJson => {
+			this.renderMap(this.worldGroup, geoJson)
+		}, e => {
+			reject(e)
+		})
+		d3.json('https://geo.datav.aliyun.com/areas_v2/bound/360000_full.json').then(geoJson => {
+			this.renderMap(this.jiangxiGroup, geoJson);
+		})
+	}
+	// 创建svg容器
+	createSVG() {
+		this.svg = d3.selectAll('svg')
+		if (this.svg._groups && this.svg._groups[0][0]) {
+			return
+		}
+		let zoom = d3.zoom()
+			.scaleExtent([1, 8])
+			.on("zoom", (e) => {
+				console.log(e)
+				this.zoomed()
+			});
+		this.svg = this.d3Container.append('svg')
+			.attr('width', this.opts.width)
+			.attr('height', this.opts.height)
+			.call(zoom)
+			.on("dblclick.zoom", null)
+			.on('click', e=>{
+				console.log(e)
+			});
+	}
+	// 创建主g容器 ，用于缩放
+	createMainGroup() {
+		this.mainGroup = this.svg.selectAll('g.main-group')
+		if (this.mainGroup._groups && this.mainGroup._groups[0][0]) {
+			return
+		}
+		this.mainGroup = this.svg.append('g')
+			.attr('class', 'main-group')
+			.on('click',e=>{
+				console.log(e)
+			})
+	}
+	// 创建地图容器，存放地图相关path
+	createMapGroup(className) {
+		let mapGroup = this.mainGroup.selectAll(`g.${className}`)
+		if (mapGroup._groups && mapGroup._groups[0][0]) {
+			return
+		}
+		return mapGroup = this.mainGroup.append('g')
+			.attr('class', className)
+	}
+	// 渲染地图
+	renderMap(mapGroup, geoJson) {
+		console.log(geoJson)
+		this.projection = d3.geoMercator().fitSize([this.opts.width - 10, this.opts.height - 10], geoJson);
+		this.path = d3.geoPath()
+			.projection(this.projection);
+		mapGroup
+			.selectAll('path')
+			.data(geoJson.features)
+			.enter()
+			// .append('g')
+			// .attr('class', 'path-group')
+			.append('path')
+			.attr('class', 'map-path')
+			.attr('d', this.path)
+			.attr('id', (d) => {
+				return `path${d.properties.adcode}`
+			}).on('click', e => {
+				console.log(e)
+				if (e.properties.adcode == 360000) {
+					this.worldGroup.attr('display', 'none');
+					this.jiangxiGroup.attr('display', 'block');
+				}
+				return false;
+			});
+		mapGroup.selectAll('text')
+			.data(geoJson.features)
+			.enter()
+			.append('text')
+			.text(d => {
+				return d.properties.name
+			})
+			.attr('transform', d => {
+				if (!d.properties.center) return
+				return `translate(${this.projection(d.properties.center)}),scale(0.8)`
+			})
+	}
+	// 缩放
+	zoomed() {
+		let t = d3.event.transform;
+
+		this.mainGroup.attr("transform", t);
+		this.mainGroup.selectAll("circle.point")
+			.transition()
+			.duration(750)
+			.attr("transform", function () {
+				let curr = this.transform.baseVal[0].matrix;
+				return "translate(" + curr.e + "," + curr.f + ")scale(" + 1 / t.k + ")";
+			});
+	}
 }
--- src/components/map.vue
+++ src/components/map.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="map-container" ref="mapContaner"></div>
+  <div class="map-container" ref="mapContaner"></div>
 </template>
 <script>
-import BaseMap from '../assets/js/baseMap'
+import BaseMap from "../assets/js/baseMap";
 export default {
-    name:'my-map',
-    data(){
-        return {
-            baseMap: null
-        }
-    },
-    methods:{
-
-    },
-    mounted(){
-        /* eslint-disable */
-        console.log(this.$refs['mapContaner'].clientHeight)
-        let options = {
-            width: this.$refs['mapContaner'].clientWidth,
-            height: this.$refs['mapContaner'].clientHeight
-        }
-        this.baseMap = new BaseMap('.map-container',options)
-        console.log(this.baseMap)
-        this.baseMap.init()
-    }
-}
+  name: "my-map",
+  data() {
+    return {
+      baseMap: null,
+    };
+  },
+  methods: {},
+  mounted() {
+    /* eslint-disable */
+    console.log(this.$refs["mapContaner"].clientHeight);
+    let options = {
+      width: this.$refs["mapContaner"].clientWidth,
+      height: this.$refs["mapContaner"].clientHeight,
+    };
+    this.baseMap = new BaseMap(".map-container", options);
+    console.log(this.baseMap);
+    this.baseMap.init();
+  },
+};
 </script>
 <style lang="scss">
-.map-container{
-    height: 100%;
-    .map-group{
-        .map-path{
-            fill: #f9f9f9;
-            stroke: #ddd;
-            stroke-width: 0.5px;
-            text{
-                fill: gray;
-            }
-        }
+.map-container {
+  height: 100%;
+  .map-group,
+	.map-group-jiangxi {
+    .map-path {
+      fill: #f9f9f9;
+      stroke: #ddd;
+      stroke-width: 0.5px;
+      text {
+        fill: gray;
+      }
     }
-}</style>
+  }
+}
+</style>
